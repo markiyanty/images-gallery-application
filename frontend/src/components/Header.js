1611aab40150a7968ec14a1f15eab7e577a8d0f3
Markiyan Tyndyk <markiyan.tyndyk@gmail.com>
2022-08-22 14:20:02 +0300

    controlled search component

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Navbar, Container } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const navBarStyle = {
+    backgroundColor: 'lightgreen'
+};
+
+const Header = ({ title }) => {
+    return (
+        <Navbar style={navBarStyle} variant="light">
+            <Container>
+                <Navbar.Brand href="/" className=""> {title}</Navbar.Brand>
+            </Container>
+        </Navbar>
+    )
+};
+
+
+export default Header;
