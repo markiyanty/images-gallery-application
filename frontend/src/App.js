7fabd4eaa8ef70061afd727d1dc5081b5e5494af
Markiyan Tyndyk <markiyan.tyndyk@gmail.com>
2022-08-24 09:50:57 +0300

    first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,26 @@ import Search from './components/Search';
 import { useState } from 'react';
 
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
+
 const App = () => {
   const [word, setWord] = useState('');
 
   const handleSearchSubmit = (e)=> {
     e.preventDefault();
     console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    .then((res)=>res.json())
+    .then((data)=>{
+      console.log(data);
+    })
+    .catch((err)=>{
+      console.log(console.log(err))
+    })
+
   }
-  
+
   return (
     <div className="App">
       <Header title={"Images Gallery"}/> 
